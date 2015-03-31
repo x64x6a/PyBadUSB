8963cda157f8813e2a92e960258e310cd6d745d9
x64x6a <darekjens@gmail.com>
2015-03-31 14:20:08 -0500

    Fixed compiler flags

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 from setuptools import Extension
+import platform
+
+if platform.system() == 'Windows':
+	scsi_compile_args = []
+else:
+	scsi_compile_args = ["-Wno-write-strings"]
+
 
 setup(	name='pybadusb',
 		version='0.2',
 		packages=['pybadusb'],
 		ext_package='pybadusb',
-		ext_modules=[Extension('scsi', ['pybadusb/src/scsi.cpp'], extra_compile_args = ["-Wno-write-strings"])],
+		ext_modules=[Extension('scsi', ['pybadusb/src/scsi.cpp'], extra_compile_args = scsi_compile_args)],
 	)
