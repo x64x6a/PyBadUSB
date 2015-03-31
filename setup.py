95c69ec5b74b3a14b99b423192b7e050f5383fc1
x64x6a <darekjens@gmail.com>
2015-03-31 13:56:34 -0500

    Fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import Extension
 
 setup(	name='pybadusb',
-		version='1.0',
+		version='0.2',
 		packages=['pybadusb'],
 		ext_package='pybadusb',
 		ext_modules=[Extension('scsi', ['pybadusb/src/scsi.cpp'], extra_compile_args = ["-Wno-write-strings"])],
