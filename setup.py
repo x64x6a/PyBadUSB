fb9029b40c308c956c31f508a9d06fc4484d8881
x64x6a <darekjens@gmail.com>
2015-03-31 02:54:44 -0500

    Fixed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 setup(	name='pybadusb',
-		version='1.0',
+		version='0.1',
 		packages=['pybadusb'],
 		ext_package='pybadusb',
 		ext_modules=[Extension('scsi', ['pybadusb/src/scsi.cpp'])],
